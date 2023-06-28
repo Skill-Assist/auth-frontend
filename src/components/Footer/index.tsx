4c888075f0244e46c7deb5d4d3c6d50d3f7ca74c
Luiz Felipe <68920578+luiz-k-alencar@users.noreply.github.com>
2023-06-28 15:20:49 -0300

    first commit

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,15 @@
+import { FC } from 'react'
+
+import styles from './styles.module.scss'
+
+const Footer: FC = () => {
+  return (
+    <div className={styles.footer}>
+      <span>
+        &copy; {new Date().getFullYear()} Skill Assist | All rights reserved
+      </span>
+    </div>
+  )
+}
+
+export default Footer
