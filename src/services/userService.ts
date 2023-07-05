819a11b572f8e3ecb4da30f48fedfc777ca2305d
Luiz Felipe <68920578+luiz-k-alencar@users.noreply.github.com>
2023-07-05 13:16:38 -0300

    update services with baseURL

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -2,7 +2,7 @@ import { User } from '@/types/user'
 import axios from 'axios'
 import cookie from 'react-cookies'
 
-const API_URL = 'http://localhost:5500/api/v1'
+const API_URL = process.env.NEXT_PUBLIC_BASE_URL
 
 const userService = {
   signIn: async (email: string, password: string) => {
