72cbba9142e072af6f862739246eebfb6eddf54a
Luiz Felipe <68920578+luiz-k-alencar@users.noreply.github.com>
2023-07-28 13:52:51 -0300

    trying with meta tag

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -3,6 +3,10 @@ export default function Head() {
     <>
       <title>Login | Skill Assist</title>
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta
+        httpEquiv="Content-Security-Policy"
+        content="upgrade-insecure-requests"
+      />
       <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
     </>
   )
